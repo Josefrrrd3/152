6fc8863db850cb24057672c17dbe6b29ce3a85fc
Josefrrrd3 <jose.frrrd3@eduge.ch>
2022-02-02 13:18:07 +0100

    correct input file

--- vues/post.php
+++ vues/post.php
@@ -16,11 +16,11 @@
                     <label name="fileToUpload" class="imageUpload" for="fileToUpload">
                         <li class="imageUpload"><i style="cursor: pointer;" class="imageUpload glyphicon glyphicon-picture"></i></li>
                     </label>
-                    <input id="fileToUpload" type="file" name="fileToUpload" style="display: none;">
+                    
                     <li><a href="#"><i class="glyphicon glyphicon-star imageUpload"></i></a></li>
                     <li><a href=""><i class="glyphicon glyphicon-globe"></i></a></li>
                     <li><a href=""><i class="glyphicon glyphicon-list-alt"></i></a></li>
-                    <input id="fileToUpload" multiple  accept=".jpg,.jpeg,.png" type="file" name="fileToUpload" style="display: none;" onchange="validateFileType()">
+                    
                 </ul>
             </div>
         </div>
